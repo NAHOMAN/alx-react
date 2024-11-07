3e445001c97e9838145a3f981a53aaeef5f0d060
2NG CODER <nahomnigussie7@gmail.com>
2024-11-07 14:50:44 +0300

    Commence with class components

--- 0x03-React_component/task_1/dashboard/src/App/App.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React, {Component } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
 import Header from '../Header/Header';
@@ -9,14 +9,14 @@ import CourseList from '../CourseList/CourseList'
 import { getLatestNotification } from '../utils/utils';
 
 const listCourses = [
-  {id: 1, name: "ES6", credit: 60},
-  {id: 2, name: "Webpack", credit: 20},
-  {id: 3, name: "React", credit: 40}
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 }
 ]
 const listNotifications = [
-  {id: 1, type:'default', value:"New course available"},
-  {id: 2, type:'urgent', value:"New resume available"},
-  {id: 3, type:'urgent', html:{__html: getLatestNotification() }}
+  { id: 1, type: 'default', value: "New course available" },
+  { id: 2, type: 'urgent', value: "New resume available" },
+  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
 ]
 
 class App extends Component {
@@ -33,15 +33,15 @@ class App extends Component {
   componentWillUnmount() {
     document.removeEventListener("keydown", this.handleKeyPress)
   }
-  render () {
+  render() {
     return (
       <React.Fragment>
         <Notifications listNotifications={listNotifications} />
         <div className="App">
-            <Header />
-            <div className="App-body">
-              {this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
-            </div>
+          <Header />
+          <div className="App-body">
+            {this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          </div>
         </div>
         <div className="App-footer">
           <Footer />
@@ -53,7 +53,7 @@ class App extends Component {
 
 App.defaultProps = {
   isLoggedIn: false,
-  logOut: () => {return }
+  logOut: () => { return }
 }
 
 App.propTypes = {
